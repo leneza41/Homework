f7332489c847a868fade0162c28b379652dba0c5
leneza41 <leonar4145@gmail.com>
2025-06-11 10:46:30 -0600

    Add project files.

---
+++ ctlAlarmClock.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace ctlClockLib
+{
+    public partial class ctlAlarmClock
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblAlarm = new Label();
+            btnAlarmOff = new Button();
+            SuspendLayout();
+            // 
+            // lblAlarm
+            // 
+            lblAlarm.AutoSize = true;
+            lblAlarm.Location = new Point(187, 10);
+            lblAlarm.Name = "lblAlarm";
+            lblAlarm.Size = new Size(65, 20);
+            lblAlarm.TabIndex = 1;
+            lblAlarm.Text = "!Alarma!";
+            lblAlarm.TextAlign = ContentAlignment.MiddleCenter;
+            lblAlarm.Visible = false;
+            // 
+            // btnAlarmOff
+            // 
+            btnAlarmOff.Location = new Point(12, 50);
+            btnAlarmOff.Name = "btnAlarmOff";
+            btnAlarmOff.Size = new Size(192, 29);
+            btnAlarmOff.TabIndex = 2;
+            btnAlarmOff.Text = "Deshabilitar alarma";
+            btnAlarmOff.UseVisualStyleBackColor = true;
+            btnAlarmOff.Click += btnAlarmOff_Click;
+            // 
+            // ctlAlarmClock
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(btnAlarmOff);
+            Controls.Add(lblAlarm);
+            Name = "ctlAlarmClock";
+            Controls.SetChildIndex(lblAlarm, 0);
+            Controls.SetChildIndex(btnAlarmOff, 0);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblAlarm;
+        private Button btnAlarmOff;
+    }
+}
---
+++ ctlClock.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace ctlClockLib
+{
+    public partial class ctlClock
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            lblDisplay = new Label();
+            timer1 = new System.Windows.Forms.Timer(components);
+            SuspendLayout();
+            // 
+            // lblDisplay
+            // 
+            lblDisplay.AutoSize = true;
+            lblDisplay.Font = new Font("Segoe UI", 14F);
+            lblDisplay.Location = new Point(0, 0);
+            lblDisplay.Name = "lblDisplay";
+            lblDisplay.Size = new Size(153, 32);
+            lblDisplay.TabIndex = 0;
+            lblDisplay.Text = "(blank space)";
+            lblDisplay.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            timer1.Interval = 1000;
+            timer1.Tick += timer1_Tick;
+            // 
+            // ctlClock
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(lblDisplay);
+            Name = "ctlClock";
+            Size = new Size(800, 450);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblDisplay;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
